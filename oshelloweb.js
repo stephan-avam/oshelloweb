7431caecf1f773c8e676a93fb61f0fcd59ccb5cb
Stephan Baumann <stephan.baumann@adesso.ch>
2018-06-23 15:00:16 +0200

    env variable PORT und MESSAGE hinzugefügt

--- oshelloweb.js
+++ oshelloweb.js
@@ -1,13 +1,14 @@
 const http = require('http');
 
 const name = 'node-hello-world';
-const port = '8080';
+const port = process.env.PORT || '8080';
+const msg = process.env.MESSAGE || 'Hello World';
 
 const app = new http.Server();
 
 app.on('request', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('Hello World');
+  res.write(msg);
   res.end('\n');
 });
 
