c8f41ca8350112eab4d2791da4cbbc015c455893
Stephan Baumann <stephan.baumann@adesso.ch>
2018-06-25 15:01:22 +0200

    connection change

--- oshelloweb.js
+++ oshelloweb.js
@@ -6,14 +6,14 @@ const port = process.env.PORT || '8080';
 const msg = process.env.MESSAGE || 'Hello World';
 
 const app = new http.Server();
-const connection = mysql.createConnection({
-  host     : process.env.MARIADB_SERVICE_HOST,
-  user     : process.env.MARIA_USER,
-  password : process.env.MARIA_SECRET,
-  database : process.env.MARIA_DBNAME
-});
 
 app.on('request', (req, res) => {
+  const connection = mysql.createConnection({
+    host     : process.env.MARIADB_SERVICE_HOST,
+    user     : process.env.MARIA_USER,
+    password : process.env.MARIA_SECRET,
+    database : process.env.MARIA_DBNAME
+  });
   connection.connect();
   connection.query('SELECT description from ici_test where id = 1', function (error, results, fields) {
     if (error) result = error;
